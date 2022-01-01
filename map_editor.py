1b980e9a467cd634d6c97e4d2d3a4712d491e624
fin93 <tiranasawrus@gmail.com>
2022-01-01 12:33:45 +0000

    finished lvl editor!

--- map_editor.py
+++ map_editor.py
@@ -14,6 +14,7 @@ is_on = False
 map_to_write = 'map_1'
 level_to_display = 'level: 1'
 display_which_level = font.render(level_to_display, True, TEXT_COLOUR)
+number_of_levels = 0
 
 grass_orig = pygame.image.load('Assets/block/grass.jpg')
 grass_block = pygame.transform.scale(grass_orig, (50, 50))
@@ -27,6 +28,25 @@ def write_map(filename, current_map):
             f.write(current_map[i][j])
         f.write('\n')
     f.close()
+
+def append_full_map(filename, full_filename):
+    x = open(filename + '.txt', 'r')
+    data = x.read()
+    x.close()
+    f = open(full_filename + '.txt', 'a+')
+    f.write('nn\n')
+    f.write(data)
+    f.close()
+
+def how_many(filename):
+    global number_of_levels
+    f = open(filename + '.txt', 'r')
+    data = f.read()
+    f.close()
+    data = data.split('nn')
+    return len(data)
+
+number_of_levels = how_many(f'maps/{map_name}')
 
 def load_full_map(filename, map_number):
     f = open(filename + '.txt', 'r')
@@ -90,13 +110,22 @@ while True:
                 text = font.render('written', True, TEXT_COLOUR)
             if event.key == pygame.K_RIGHT:
                 map_level_number += 1
-                game_map = load_full_map('maps/full_map', map_level_number)
-                level_to_display = f'level: {map_level_number + 1}'
+                game_map = load_full_map(f'maps/{map_name}', map_level_number % number_of_levels)
+                level_to_display = f'level: {(map_level_number % number_of_levels) + 1}'
                 display_which_level = font.render(level_to_display, True, TEXT_COLOUR)
             elif event.key == pygame.K_LEFT:
-                map_level_number -= 1
-                game_map = load_full_map('maps/full_map', map_level_number)
-                level_to_display = f'level: {map_level_number + 1}'
+                if map_level_number > 0:
+                    map_level_number -= 1
+                game_map = load_full_map(f'maps/{map_name}', map_level_number % number_of_levels)
+                level_to_display = f'level: {(map_level_number % number_of_levels) + 1}'
+                display_which_level = font.render(level_to_display, True, TEXT_COLOUR)
+            if event.key == pygame.K_a:
+                map_level_number = 0
+                append_full_map(f'maps/{map_to_write}', f'maps/{map_name}')
+                text = font.render('written full_map', True, TEXT_COLOUR)
+                game_map = load_full_map(f'maps/{map_name}', map_level_number)
+                number_of_levels = how_many(f'maps/{map_name}')
+                level_to_display = f'level: {(map_level_number % number_of_levels) + 1}'
                 display_which_level = font.render(level_to_display, True, TEXT_COLOUR)
         if event.type == pygame.MOUSEBUTTONDOWN:
             is_on = True
